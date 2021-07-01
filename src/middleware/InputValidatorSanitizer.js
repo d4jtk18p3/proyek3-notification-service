7146b8187318f65e8252e20bc12636065083cf11
RayhanAnandhias <48201126+RayhanAnandhias@users.noreply.github.com>
2021-07-02 06:55:01 +0700

    refactor

--- src/middleware/InputValidatorSanitizer.js
+++ src/middleware/InputValidatorSanitizer.js
@@ -1,11 +1,25 @@
 import { body, param } from 'express-validator'
 
 export const createUserDevice = [
-    body('user_id', 'User id tidak boleh kosong').exists(),
-    body('token', 'Token tidak boleh kosong').exists()
+  body('user_id', 'User id tidak boleh kosong').exists(),
+  body('token', 'Token tidak boleh kosong').exists()
 ]
 
 export const addUserToGroup = [
-    body('userIdList', 'User id tidak boleh kosong').isArray().exists(),
-    body('groupName', 'Nama group tidak boleh kosong').exists()
+  body('userIdList', 'User id tidak boleh kosong').isArray().exists(),
+  body('groupName', 'Nama group tidak boleh kosong').exists()
+]
+
+export const sendEmailGroupNotification = [
+  body('groupName')
+    .trim()
+    .notEmpty()
+    .withMessage('Nama grup tidak boleh kosong')
+    .isString()
+    .withMessage('Nama grup harus bertipe string'),
+  body('subject')
+    .trim()
+    .notEmpty()
+    .withMessage('Subjek email tidak boleh kosong'),
+  body('subject').trim().notEmpty().withMessage('Body email tidak boleh kosong')
 ]
