f7498e0f79802e1d8ae84d55e888439bf3856269
Rayhan Anandhias <48201126+RayhanAnandhias@users.noreply.github.com>
2021-07-01 17:22:00 +0700

    Task/init (#1)
    * init server
    
    * add firebase admin
    
    * put firebase admin in src

---
+++ src/server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+// error handling
+app.use((error, req, res, next) => {
+  console.log(error)
+  const status = error.statusCode || 500
+  const message = error.message
+  const cause = error.cause || 'Internal Server Error'
+  res.status(status).json({
+    message: message,
+    error: status,
+    cause: cause
+  })
+})
+
+export default app
